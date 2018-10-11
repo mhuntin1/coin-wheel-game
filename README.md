f6283a17551738032b7b958dee0c7bd7e2fbaf9e
applejaxx <applejaxx@users.noreply.github.com>
2018-10-10 20:57:07 -0600

    Jaxx (#4)
    * Mhuntin1 (#2) (#1)
    
    * Runner, ConsoleGame, and ConsoleGameTest were added
    
    * java doc comments and complete ConsoleGame methods
    
    * remove test files
    
    * add strategic player interface, add documents folder
    
    * add build xml file for ant
    
    * Update README.md
    
    * Javadoc, code added to run console

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # coin-wheel-game
-A Game for fun and anlysis.
+A Game for fun and analysis.
 
 Client instructions such as ant tasks here
--- coin-wheel-game-intellij/src/Runner.java
+++ coin-wheel-game-intellij/src/Runner.java
@@ -1,6 +1,17 @@
 public class Runner {
+
+
+    /**
+     * Main method to play game through the console
+     * @author Jon Bowen, Jackie Nugent, Mark Huntington
+     * @version 0.0.2
+     */
     public static void main( String [] args){
 
+            ConsoleGame consoleGame = new ConsoleGame();
+
+            consoleGame.start();
 
     }
+
 }
